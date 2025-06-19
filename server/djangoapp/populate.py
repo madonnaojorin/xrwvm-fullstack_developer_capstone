9a24299510b2066e60cdf82763a0898b14fc80f7
Nao Yamamoto <64042180+madonnaojorin@users.noreply.github.com>
2025-06-18 20:18:54 -0400

    Update populate.py

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -3,8 +3,6 @@ from .models import CarMake, CarModel
 def initiate():
     print("Populate not implemented. Add data manually")
 
-
-def initiate():
     car_make_data = [
         {"name": "NISSAN", "description": "Great cars. Japanese technology"},
         {"name": "Mercedes", "description": "Great cars. German technology"},
